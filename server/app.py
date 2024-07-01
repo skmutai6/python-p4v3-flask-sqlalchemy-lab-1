ec1746b412646691294c75f8065602ee26be0d69
Shadrack <skmutai6@gmail.com>
2024-07-01 08:52:22 +0300

    commit

--- server/app.py
+++ server/app.py
@@ -3,6 +3,7 @@
 
 from flask import Flask, make_response
 from flask_migrate import Migrate
+from models import Earthquake
 
 from models import db, Earthquake
 
@@ -21,6 +22,29 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def filter_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id == id).first()
+
+    if earthquake:
+        body = earthquake.to_dict()
+        status = 200
+    else:
+        body = {'message': f'Earthquake {id} not found.'}
+        status = 404
+
+    return make_response(body, status)
+
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquake_by_magnitude(magnitude):
+    earthquakes = []
+    for quakes in Earthquake.query.filter_by(magnitude=magnitude).all():
+        earthquakes.append(quakes.to_dict())
+    body = {'count': len(earthquakes),
+            'quakes': earthquakes
+            }
+    return make_response(body, 200)
 
 
 if __name__ == '__main__':
